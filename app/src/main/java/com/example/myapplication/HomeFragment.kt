e8405ea866138d14bb4afc0be317287b3f43acad
Kavya Mittal <mister1729x@gmail.com>
2025-08-13 19:35:09 +0530

    Recycler view setup

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,29 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listMembers = listOf<MemberModel>(
+            MemberModel("Kavya"),
+            MemberModel("Rahul"),
+            MemberModel("Krish"),
+            MemberModel("Ben10"),
+            MemberModel("keven11"),
+            MemberModel("Gwen"),
+            MemberModel("goku"),
+            MemberModel("vegeta"),
+            MemberModel("Broly!"),
+        )
+
+        val adapter = MemberAdapter(listMembers)
+
+        val recycler = requireView().findViewById<RecyclerView>(R.id.recycler_member)
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapter
+
     }
 
     companion object {
---
+++ app/src/main/java/com/example/myapplication/MemberModel.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+data class MemberModel(
+    val name: String
+)
