a6aeaa6ce1f2acce91ff12f872a544814f5caa62
Kavya Mittal <mister1729x@gmail.com>
2025-08-16 17:52:39 +0530

    implementying room -database

---
+++ app/src/main/java/com/example/myapplication/MyFamilyDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ContactModel::class], version = 1, exportSchema = false)
+public abstract class MyFamilyDatabase: RoomDatabase() {
+
+    abstract fun contactDao(): ContactDao
+
+
+
+    companion object{
+        @Volatile
+        private var INSTANCE: MyFamilyDatabase? = null
+
+//        fun getDatabase(context: Context): MyFamilyDatabase{
+//            INSTANCE?.let { return it }
+//        }
+
+//        val instance = Room.databaseBuilder(context.applicationContext, MyFamilyDatabase::class.java,"my_family_db").build()
+    }
+}
